170eb4878513d04e5004951101353ed014c9aa1d
August <augustjp@live.dk>
2024-04-16 09:57:20 +0200

    checking if props work

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -12,6 +12,16 @@ export const Home = () => {
             voluptatem. Aut labore ab odit voluptate repellat accusantium?"
       linkName="Læs mere"
             />
+      <HomeNews title="Fernisering!" 
+      text="Kom til fernis på galleriet på lørdag det bliver sjoovt!!"
+      linkName="Læs mere"
+            />
+      <HomeNews title="Yoooo" 
+      text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur
+            adipisci magnam neque ipsum assumenda nisi impedit maxime
+            voluptatem. Aut labore ab odit voluptate repellat accusantium?"
+      linkName="Læs mere"
+            />
     </>
   );
 };
